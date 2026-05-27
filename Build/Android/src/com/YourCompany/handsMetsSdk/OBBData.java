9ad04242f60eedd2542c00bf684aecf954a303d6
ShadowBonn21 <maximyarohovich21@gmail.com>
2026-05-27 18:20:42 +0300

    prefinal

--- Build/Android/src/com/YourCompany/handsMetsSdk/OBBData.java
+++ Build/Android/src/com/YourCompany/handsMetsSdk/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-958340719L // the length of the file in bytes
+963173471L // the length of the file in bytes
 )
 };
 };
